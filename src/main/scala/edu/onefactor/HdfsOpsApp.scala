215b21ef0317a941eff347d18e95b3551f992449
Andrey Smirnov <andrey.smirnov@onefactor.com>
2020-12-29 09:17:52 +0300

    logging directly to HDFS

--- src/main/scala/edu/onefactor/HdfsOpsApp.scala
+++ src/main/scala/edu/onefactor/HdfsOpsApp.scala
@@ -4,6 +4,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.fs.FileSystem
 import java.net.URI
 import org.apache.hadoop.fs.Path
+import java.util.UUID
+import java.io.OutputStreamWriter
 
 object HdfsOpsApp {
     def main(args: Array[String]): Unit = {
@@ -22,6 +24,24 @@ object HdfsOpsApp {
         // fails with exception due to missing permissions
         // val dirCreated = fs.mkdirs(new Path("/tmp/hadoop-yarn/testdir"))
 
+        createLogFile(fs)
+    }
+
+    private def createLogFile(fs: FileSystem): Unit = {
+        println("creating new log file")
+
+        val logFileName = UUID.randomUUID().toString() + ".log"
+
+        val logFileOut = fs.create(new Path(s"/shared/andrey.smirnov/$logFileName"))
+        val logFileWriter = new OutputStreamWriter(logFileOut)
+
+        logFileWriter.write("first debug message")
+
+        logFileWriter.close()
+        logFileOut.close()
+    }
+
+    private def createDir(fs: FileSystem): Unit = {
         val dirCreated = fs.mkdirs(new Path("/shared/andrey.smirnov/test-entry"))
 
         println(s"dir created: $dirCreated")
