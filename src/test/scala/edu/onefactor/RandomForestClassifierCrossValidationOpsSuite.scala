2e2bc21f4d18a8b76b65e41cc70cacd51b5e654a
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-04-27 18:28:05 +0300

    random forest cross-validated model selection

---
+++ src/test/scala/edu/onefactor/RandomForestClassifierCrossValidationOpsSuite.scala
@@ -0,0 +1,145 @@
+package edu.onefactor
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{OneHotEncoder, OneHotEncoderEstimator, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+class RandomForestClassifierCrossValidationOpsSuite extends FunSuite with BeforeAndAfterAll {
+  lazy val spark = SparkSession.builder()
+    .appName("test")
+    .master("local[*]")
+    .config("spark.driver.allowMultipleContexts", "false")
+    .getOrCreate()
+
+  test("random forest classifier demo") {
+    val trainCategorical = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("file:///Users/andrey.smirnov/Downloads/kaggle-bosch/train_categorical.csv")
+
+    import org.apache.spark.sql.functions._
+
+    showDataset("train categorical w/o nulls", trainCategorical) { df =>
+      df.where(col("L0_S22_F545").isNotNull).select("Id", "L0_S22_F545")
+    }
+
+    val trainNumeric = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("file:///Users/andrey.smirnov/Downloads/kaggle-bosch/train_numeric.csv")
+
+    // showDataset("train numeric data", trainNumeric)()
+
+    val trainData = trainCategorical
+      .join(trainNumeric, "Id")
+      .select(
+        col("Response").as("label"),
+        coalesce(col("L0_S22_F545"), lit("NA")).as("L0_S22_F545"),
+        coalesce(col("L0_S0_F0"), lit(0.0)).as("L0_S0_F0"),
+        coalesce(col("L0_S0_F2"), lit(0.0)).as("L0_S0_F2"),
+        coalesce(col("L0_S0_F4"), lit(0.0)).as("L0_S0_F4"))
+
+    showDataset("train dataset w/o nulls", trainData)()
+
+    val indexer = new StringIndexer()
+      .setHandleInvalid("skip")
+      .setInputCol("L0_S22_F545")
+      .setOutputCol("L0_S22_F545Index")
+
+    // val trainDataIndexed = indexer.fit(trainData).transform(trainData)
+
+    // showDataset("train data indexed", trainDataIndexed)(_.where(col("L0_S22_F545") =!= "NA"))
+
+    val encoder = new OneHotEncoderEstimator()
+      .setInputCols(Array("L0_S22_F545Index"))
+      .setOutputCols(Array("L0_S22_F545Encoded"))
+
+    // val trainDataEncoded = encoder.transform(trainDataIndexed)
+
+    // showDataset("train data encoded", trainDataEncoded)()
+
+    val vectorAssembler = new VectorAssembler()
+      .setInputCols(Array("L0_S22_F545Encoded", "L0_S0_F0", "L0_S0_F2", "L0_S0_F4"))
+      .setOutputCol("features")
+
+    // val trainDataVectorized = vectorAssembler.transform(trainDataEncoded)
+
+    // showDataset("train data vectorized", trainDataVectorized)()
+
+    val randomForestClassifier = new RandomForestClassifier()
+      .setLabelCol("label")
+      .setFeaturesCol("features")
+
+    val pipeline = new Pipeline()
+      .setStages(Array(indexer, encoder, vectorAssembler, randomForestClassifier))
+
+    val pipelineModel = pipeline.fit(trainData)
+
+    val result = pipelineModel.transform(trainData)
+
+    showDataset("result", result) { df =>
+      // df.where(col("label") === lit(1))
+      df.where(col("prediction") =!= 0.0)
+    }
+
+    // displaying model parameters
+    for (pipelineModelStage <- pipelineModel.stages) {
+      println("pipeline stage: " + pipelineModelStage)
+    }
+
+    val randomForestClassifierModel = pipelineModel.stages.last
+      .asInstanceOf[RandomForestClassificationModel]
+
+    println(s"random forest classifier model: num trees = ${randomForestClassifierModel.getNumTrees}, " +
+      s"feature subset strategy = ${randomForestClassifierModel.getFeatureSubsetStrategy}, " +
+      s"impurity = ${randomForestClassifierModel.getImpurity}, " +
+      s"max bins = ${randomForestClassifierModel.getMaxBins}, " +
+      s"max depth = ${randomForestClassifierModel.getMaxDepth}")
+
+    val evaluator = new BinaryClassificationEvaluator()
+      .setMetricName("areaUnderROC") // ROC AUC ( area under curve )
+      .setRawPredictionCol("prediction")
+      .setLabelCol("label")
+
+    val randomForestClassifierParams = new ParamGridBuilder()
+      .addGrid(randomForestClassifier.numTrees, Array(10, 20))
+      .addGrid(randomForestClassifier.featureSubsetStrategy, Array("auto", "all"))
+      .addGrid(randomForestClassifier.impurity, Array("gini"))
+      .addGrid(randomForestClassifier.maxBins, Array(32))
+      .addGrid(randomForestClassifier.maxDepth, Array(5, 7))
+      .build()
+
+    val crossValidator = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(randomForestClassifierParams)
+      .setNumFolds(4)
+
+    val crossValidatorModel = crossValidator.fit(trainData)
+
+    val optimalPipelineModel = crossValidatorModel.bestModel
+
+    val optimalResult = crossValidatorModel.transform(trainData)
+
+    showDataset("best model result", optimalResult)()
+  }
+
+  override def afterAll(): Unit = {
+    spark.stop()
+  }
+
+  private def showDataset(
+                           name: String,
+                           df: DataFrame,
+                           numRows: Int = 20)
+                         (transformer: DataFrame => DataFrame = identity): Unit = {
+
+    println(s"dataset: $name with schema: ${df.schema}")
+
+    transformer(df).show(numRows = numRows, truncate = false)
+  }
+}
